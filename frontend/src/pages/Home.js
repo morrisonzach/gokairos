d12e5bcd70d62a85de0bdb5a4bb6ba5c32053641
zach <ziemboy9@gmail.com>
2020-11-27 04:02:11 -0600

    still making pretty slow progress

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,33 +1,40 @@
 import Carousel from './../components/Carousel';
 import imgs from './../components/HomeImgArray';
+import './Home.css';
 
 function Home() {
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        cssEase: "linear",
         slidesToShow: 1,
         slidesToScroll: 1
     }
     const slidesInfo = [
         {
+            id: 0,
             title: "1",
             body: "lorem ipsum dolor sit amet",
             img: imgs[0]
         },
         {
+            id: 1,
             title: "2",
             body: "lorem ipsum dolor sit amet",
             img: imgs[1]
         },
         {
+            id: 2,
             title: "3",
             body: "lorem ipsum dolor sit amet",
             img: imgs[2]
         }
     ];
     const slides = slidesInfo.map((slide) => (
-        <div>
+        <div key={slide.id}>
             <h1>
                 {slide.title}
             </h1>
